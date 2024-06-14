bbff2841db31dfdbc2e8bbc716033ecb3688f5e0
Leandro Lopes <lslopes.dev@gmail.com>
2024-06-14 11:46:21 -0300

    Initial Commit

---
+++ PDFTextExtractor/PDFTextExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PDFTextExtractorApp.swift
+//  PDFTextExtractor
+//
+//  Created by Leandro Lopes on 14/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PDFTextExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
