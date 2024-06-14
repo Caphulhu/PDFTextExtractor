85f354ed82668d476da4a8da42203a6b22ff337c
Leandro Lopes <lslopes.dev@gmail.com>
2024-06-14 15:04:14 -0300

    Wip

--- PDFTextExtractor/PDF/PDFDragDropView.swift
+++ PDFTextExtractor/PDF/PDFDragDropView.swift
@@ -11,9 +11,25 @@ import PDFKit
 struct PDFDragDropView: View {
     @State private var pdfURL: URL?
     @State private var pdfDocument: PDFDocument?
-
+    @State private var pdfStrings: [String] = []
+    @State private var isPDFRead: Bool = false
+    @State private var showPDFStringsView: Bool = false
+    
     var body: some View {
+        if showPDFStringsView {
+            PDFStringsView(title: "Valores", strings: pdfStrings)
+        } 
+
         VStack {
+            if isPDFRead {
+                Button("Ver Valores") {
+                    showPDFStringsView.toggle()
+                }
+                .padding(.top, 20)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            }
             Text("Drag and Drop PDF Here")
                 .onDrop(of: ["public.file-url"], isTargeted: nil) { providers -> Bool in
                     if let item = providers.first {
@@ -30,12 +46,30 @@ struct PDFDragDropView: View {
                     }
                     return true
                 }
-
-            if let pdfDocument = pdfDocument {
-                PDFKitView(url: pdfDocument.documentURL!)
+            
+            if let pdfDocument = pdfDocument, let url = pdfDocument.documentURL {
+                PDFKitView(url: url)
+                    .padding(.top, 20)
+                    .onAppear(perform: readPDFStrings)
             } else {
                 Text("No PDF Selected")
             }
+        }
+    }
+    
+    func readPDFStrings() {
+        // Read the PDF strings
+        if let pdfDocument = pdfDocument {
+            for i in 0..<pdfDocument.pageCount {
+                if let page = pdfDocument.page(at: i) {
+                    if let pageContent = page.attributedString {
+                        let string = pageContent.string
+                        pdfStrings.append(string)
+                    }
+                }
+            }
+            isPDFRead = true
+            debugPrint(pdfStrings)
         }
     }
 }
--- PDFTextExtractor/Strings/PDFStringsView.swift
+++ PDFTextExtractor/Strings/PDFStringsView.swift
@@ -5,4 +5,31 @@
 //  Created by Leandro Lopes on 14/06/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct PDFStringsView: View {
+    let title: String
+    let strings: [String]
+
+    var body: some View {
+        VStack {
+            Text(title)
+                .font(.largeTitle)
+                .padding(.top, 20)
+
+            List(strings, id: \.self) { string in
+                Text(string)
+            }
+        }
+    }
+}
+
+struct PDFStringsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PDFStringsView(title: "Valores", strings: ["String 1", "String 2", "String 3", "String 4"])
+    }
+}
+
+#Preview {
+    PDFDragDropView()
+}
